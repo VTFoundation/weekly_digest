edf78974bfb6977991bda1ff070b7404d6a7c91e
Abdulrahman Olagunju <48863727+BodeRahman@users.noreply.github.com>
2022-09-16 18:28:52 +0100

    Update VTF Live

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,16 @@ const weekData = [
                     {
                         title: "Topics",
                         html: `
+                            <div style="position: relative; width: 80%; height: 0; padding-top: 56.2500%;
+                            padding-bottom: 48px; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
+                            border-radius: 8px; will-change: transform;">
+                                <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+                                    src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFMXPSGSEE&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+                                </iframe>
+                            </div>
+                            <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFMXPSGSEE&#x2F;view?utm_content=DAFMXPSGSEE&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">VTF_LIVE_Sep 16</a> by Victor Monga
+                            <br>
+                            <br>
                             <li class="inside-text">
                                 <p>MITRE ATTACK - <a href="https://attack.mitre.org/">https://attack.mitre.org/</a></p>
                                 <ol type=a>
