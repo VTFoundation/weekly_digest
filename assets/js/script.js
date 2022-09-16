7332b92d520d4d1f39b593a3b4bbd1795b78a7d6
Abdulrahman Olagunju <48863727+BodeRahman@users.noreply.github.com>
2022-09-16 01:01:50 +0100

    Added countdown for VTF Live

--- assets/js/script.js
+++ assets/js/script.js
@@ -1298,3 +1298,32 @@ $(document).mouseup(function(e) {
         closeModal();
     }
 });
+
+// Set the date we're counting down to
+var countDownDate = new Date("Sep 16, 2022 17:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML ="Next VTF Live in " + days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "Click to join VTF LIVE";
+  }
+}, 1000);
