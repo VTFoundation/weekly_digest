40d908e81113db9a28a5036d95315b2244b99e22
Abdulrahman Olagunju <48863727+BodeRahman@users.noreply.github.com>
2022-09-19 18:57:13 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -220,16 +220,9 @@ const weekData = [
                     {
                         title: "Topics",
                         html: `
-                            <div style="position: relative; width: 80%; height: 0; padding-top: 56.2500%;
-                            padding-bottom: 48px; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
-                            border-radius: 8px; will-change: transform;">
-                                <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
-                                    src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFMXPSGSEE&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
-                                </iframe>
-                            </div>
-                           
+                            
                             <a role="button" style="padding:2px 2px; font-size: 1rem; border-radius: 8px; margin-left: 25%; color: #fff; text-decoration: none; background-color: #0d6efd; font-weight:400; border-color: #0d6efd;" 
-                            href="https://youtu.be/zO0Ht5DPybc" target="_blank">VTF-LIVE Sep 16</a>
+                            href="https://youtu.be/zO0Ht5DPybc" target="_blank">VTF-LIVE Sep 23</a>
                             <br>
                             <br>
                             <li class="inside-text">
