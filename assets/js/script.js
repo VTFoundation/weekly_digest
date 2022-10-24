d3d02e767a4047376439ff4e104a0c98ecf239f3
Abdulrahman Olagunju <abdulrahmanolagunju.1@gmail.com>
2022-10-24 14:10:33 +0100

    Update week 7 date

--- assets/js/script.js
+++ assets/js/script.js
@@ -903,7 +903,7 @@ const weekData = [
                         title: "Topics",
                         html: `
                             <a role="button" style="padding:2px 2px; font-size: 1rem; border-radius: 8px; margin-left: 25%; color: #fff; text-decoration: none; background-color: #0d6efd; font-weight:400; border-color: #0d6efd;" 
-                            href="https://www.linkedin.com/company/vtfoundation/mycompany/" target="_blank">VTF-LIVE Oct 7</a>
+                            href="https://www.linkedin.com/company/vtfoundation/mycompany/" target="_blank">VTF-LIVE Oct 28</a>
                             <br>
                             <br>
                             <li class="inside-text">
