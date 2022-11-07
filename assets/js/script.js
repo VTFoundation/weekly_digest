f078410dd3b46fa0e56b2005c914595a8c2a5dcf
Abdulrahman Olagunju <48863727+BodeRahman@users.noreply.github.com>
2022-11-07 07:51:12 +0100

    Update week 9

--- assets/js/script.js
+++ assets/js/script.js
@@ -1073,6 +1073,10 @@ const weekData = [
                     {
                         title: "Topics",
                         html: `
+                            <a role="button" style="padding:2px 2px; font-size: 1rem; border-radius: 8px; margin-left: 25%; color: #fff; text-decoration: none; background-color: #0d6efd; font-weight:400; border-color: #0d6efd;" 
+                            href="https://www.linkedin.com/company/vtfoundation/mycompany/" target="_blank">VTF-LIVE Nov 11</a>
+                            <br>
+                            <br>
                             <li class="inside-text">
                                 <p><a href="https://m.youtube.com/c/LearnCantrill/playlists">https://m.youtube.com/c/LearnCantrill/playlists</a></p>
                             </li>
@@ -1341,7 +1345,7 @@ $(document).mouseup(function(e) {
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("Nov 4, 2022 17:00:00").getTime();
+var countDownDate = new Date("Nov 11, 2022 17:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
